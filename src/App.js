25c3c1b21ad03434cf47cb31b9dc0ef027384312
Hugo Vila <hvila3@gmail.com>
2017-12-13 20:28:33 +0100

    feat: List All Books

--- src/App.js
+++ src/App.js
@@ -2,22 +2,57 @@ import React, { Component } from 'react';
 import logo from './icons/logo.svg';
 import './App.css';
 
+import ListBooks from './ListBooks';
+import * as BooksAPI from './BooksAPI';
+
+
+
 
 class App extends Component {
 
+  state = {
+    books: []
+  }
+
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+      console.log(this.state)
+    })
+  }
+
+  changeShelf = (book, index, shelf) => {
+    let updatedBooks = this.state.books.slice();
+    updatedBooks[index].shelf = shelf;
+
+    this.setState((state) => ({
+      books: updatedBooks
+    }));
+
+    BooksAPI.update(book, shelf);
+  }
+
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">MyReads</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <div className="list-books">
+          <header className="list-books-title">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>MyReads</h1>
+          </header>
+          <ListBooks
+            className="list-books-content"
+            books={this.state.books}
+            onChangeShelf={this.changeShelf}
+          />
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/ListBooks.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+
+class ListBooks extends React.Component {
+
+
+
+  render() {
+
+    return (
+      <div>
+
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Currently Reading</h2>
+          <div className="bookshelf-books">
+
+            <ol className="books-grid">
+
+              {this.props.books.map((book, index) => (
+                <li key={book.id}>
+                  <div className="book">
+                    <div className="book-top">
+
+                      <div className="book-cover" style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
+
+
+                        <div className="book-shelf-changer">
+                          <select value={book.shelf} onChange={(event) => this.props.onChangeShelf(book, index, event.target.value)}>
+                            <option value="none" disabled>Move to...</option>
+
+                              <option value="currentlyReading">Currently Reading</option>
+
+                              <option value="wantToRead">Want to Read</option>
+
+                              <option value="read">Read</option>
+
+                              <option value="none">None</option>
+
+                          </select>
+                        </div>
+
+                      </div>
+
+
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">{book.authors[0]}</div>
+                    </div>
+                  
+                </li>
+              ))}
+
+            </ol>
+
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+ListBooks.propTypes = {
+  books: PropTypes.array.isRequired,
+  onChangeShelf: PropTypes.func.isRequired
+}
+
+export default ListBooks
